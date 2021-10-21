726f5323a693c39ef0f7c9f9db41ea56e3926fba
Denir <denir-junior19@live.com>
2021-10-21 20:26:34 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'mfe1',
+    loadChildren: () => 
+      import('mfe1/MfefeatureModule').then((m) => {
+        return m.MfefeatureModule;
+      }),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
